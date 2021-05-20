d5cd3b17aff66550bb5294bacbf284f4ad066fc5
Sorin Avram <sorinace@gmail.com>
2021-05-20 10:19:11 +0300

    Change the settings for ENVERON

--- psihotest/settings.py
+++ psihotest/settings.py
@@ -14,26 +14,20 @@ import os
 import django_heroku
 from django.core.exceptions import ImproperlyConfigured
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Add the password and Key in setting
-with open(os.path.join(BASE_DIR, 'secrets.json')) as secrets_file:
-    secrets = json.load(secrets_file)
-
-def get_secret(setting, secrets=secrets):
-    """Get secret setting or fail with ImproperlyConfigured"""
-    try:
-        return secrets[setting]
-    except KeyError:
-        raise ImproperlyConfigured("Set the {} setting".format(setting))
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+print(os.getenv("EMAIL_HOST_USER"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_secret('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -90,19 +84,19 @@ WSGI_APPLICATION = 'psihotest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': get_secret('NAME') ,
-        'HOST': get_secret('HOST'),
-        'USER': get_secret('USER'),
-        'PASSWORD': get_secret('DB_PASSWORD'),
-        'PORT': get_secret('PORT'),
+        'NAME': os.getenv('NAME') ,
+        'HOST': os.getenv('HOST'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'PORT': os.getenv('PORT'),
     }
 }
 
 
 # email settings:
-EMAIL_HOST = get_secret('EMAIL_HOST') 
-EMAIL_HOST_USER = get_secret('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = get_secret('EMAIL_HOST_PASSWORD')
+EMAIL_HOST = os.getenv('EMAIL_HOST') 
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 

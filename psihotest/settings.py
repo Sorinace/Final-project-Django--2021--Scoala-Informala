b69b45f2049c216e86732895d765b9c2dc062c0d
Sorin Avram <sorinace@gmail.com>
2021-05-20 12:42:00 +0300

    Add a migration

--- psihotest/settings.py
+++ psihotest/settings.py
@@ -19,9 +19,8 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Add the password and Key in setting
+# Add the password and Key in os.environ
 load_dotenv(os.path.join(BASE_DIR, '.env'))
-print(os.getenv("EMAIL_HOST_USER"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/

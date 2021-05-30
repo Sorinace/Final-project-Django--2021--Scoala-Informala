6ca7f8427a2cc02cb5fe9fe18256463e16c7700c
Sorin Avram <sorinace@gmail.com>
2021-05-30 18:53:15 +0300

    Add from PsihoTest only the name not the object

--- query/forms.py
+++ query/forms.py
@@ -4,14 +4,14 @@ import datetime
     
 class AssignPsihoTest(forms.Form):
     tests = []
-    # incarc denumirea testelor
-    psTests = PsihoTest.objects.all()
+    # incarc denumirea testelor, aduc doar denumirea nu tot obiectul
+    psTests = PsihoTest.objects.all().values('text')
     for item in psTests:
-        tests.append(item.text)
+        tests.append(item['text'])
     
     psihotest = forms.ChoiceField(label='Se atribuie testul', choices=tuple([(name, name) for name in tests]))
     name = forms.CharField(label='Pentru', max_length=100) 
     email = forms.EmailField(label='Cu adresa de e-mail') 
-    # data dupa care testul nu mai poate fi accesat, security reason!
+    # data dupa care testul nu mai poate fi accesat, de obicei 14 zile de la atribuire, security reason!
     data = forms.DateField(label='E valabil pana la data', initial=(datetime.date.today() + datetime.timedelta(days=14)), widget=forms.SelectDateWidget) 
     message = forms.CharField(label='Mesaj (opt.)', max_length=255, required=False)

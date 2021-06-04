dde58653225b2b80d17cd53997811d1c126482a6
Sorin Avram <sorinace@gmail.com>
2021-06-04 13:46:34 +0300

    Clock heroku 2

--- query/clock.py
+++ query/clock.py
@@ -1,8 +1,8 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.blocking import BackgroundScheduler
 
 from .email import sendEmailRemainder
 
-sched = BlockingScheduler()
+sched = BackgroundScheduler()
 
 # @sched.scheduled_job('interval', minutes=3)
 # def timed_job():
@@ -13,10 +13,10 @@ sched = BlockingScheduler()
 # def scheduled_job():
 #     print('This job is run every weekday at 5pm.')
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=8)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=7)
 def scheduled_job():
     sendEmailRemainder()
-    print('This send e-mail every weekday at 8am.')
+    print('This send e-mail every weekday at 7am.')
 
 @sched.scheduled_job('interval', minutes=10)
 def timed_job():

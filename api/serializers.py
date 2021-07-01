b430475447c4ebb795bed8d8b05be442b0633fe7
Sorin Avram <sorinace@gmail.com>
2021-07-01 11:44:47 +0300

    Add conection for psihotest

--- api/serializers.py
+++ api/serializers.py
@@ -38,4 +38,4 @@ class AssignedSerializers(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = AssignedTest
-        fields = ('id', 'data', 'message', 'email', )
+        fields = ('id', 'data', 'message', 'email', 'psihotest',)
